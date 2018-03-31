fe0c00185b0959e49636355b1fe4894aeb895067
Peter Simons <simons@cryp.to>
2018-03-31 20:22:56 +0200

    simple-syslog.c: improve documentation

--- c-bits/simple-syslog.c
+++ c-bits/simple-syslog.c
@@ -1,24 +1,24 @@
 #include <syslog.h>
 
 /*
-   A variant of syslog(3) that provides a simplified API to addresses the
+   A variant of syslog(3) that provides a simplified API to address the
    following issues:
 
-   - Calling variadic functions via FFI kind-of sort-of works, but the Haskell
-     standard actually doesn't guarantee it. There is a GHC extension, CApiFFI,
-     which addresses this issue, but then that extension isn't part of any
+   - Calling variadic functions via FFI kind-of "just works", but the Haskell
+     standard actually doesn't guarantee that it does. There's a GHC extension,
+     CApiFFI, which addresses this issue, but that extension isn't part of any
      Haskell standard either.
 
-   - Strings in Haskell are almost never terminated by a \0 byte. We generally
-     do know their lengths, though, so it's more convenient to specify an
-     explicit maximum field length in the format string via "%.*s" and pass our
-     string as an argument to that. An added benefit is that our syslog
-     function doesn't interpret any of those freaky % formatting features that
-     can't support (and don't want to, really).
+   - Strings in Haskell are almost never terminated by a \0 byte, but we tend
+     to know their length, so it's more convenient (and more efficient) to
+     specify an explicit maximum field length in the format string via "%.*s"
+     and pass our string as an argument to that. A welcome side-effect of this
+     approach is that the call won't try to interpret any of those freaky %
+     formatting features that we can't support (and don't want to, really).
 
-   Note that we totally don't make any effort whatsoever to guarantee
-   meaningful argument values. If you wan to pass a negative string length,
-   null pointers and non-existent  facility values ... be our guest!
+   Note that this function makes no effort to verify the validity of its
+   arguments. If you want to pass a negative string length, null pointers, and
+   non-existent facilities here ... have at it and see what happens!
  */
 
 void simpleSyslog(int facility, int priority, const char * buf, int len)
